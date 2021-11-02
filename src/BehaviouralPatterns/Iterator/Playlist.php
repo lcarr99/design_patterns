cf5e80fadcbd89d8e45a3a6816d4b606adc0c367
lcarr99 <carrdrfc99@gmail.com>
2021-11-02 23:31:28 +0000

    added iterator pattern

---
+++ src/BehaviouralPatterns/Iterator/Playlist.php
@@ -0,0 +1,48 @@
+<?php
+
+class Playlist implements Iterator
+{
+    private int $key;
+    private array $songList;
+    private ?int $playing = null;
+
+    public function __construct(array $songList)
+    {
+        $this->songList = $songList;
+    }
+
+    public function play(): void
+    {
+        $this->playing = $this->key;
+    }
+
+    public function pause(): void
+    {
+        $this->playing = null;
+    }
+
+    public function key()
+    {
+        return $this->key;
+    }
+
+    public function next()
+    {
+        $this->key++;
+    }
+
+    public function rewind()
+    {
+        $this->key = 0;
+    }
+
+    public function current()
+    {
+        return $this->key;
+    }
+
+    public function valid()
+    {
+        return $this->key >= count($this->songList);
+    }
+}
